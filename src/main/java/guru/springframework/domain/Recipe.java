956450f8f1f1467ada2f4b3b8649101a36686a47
basly <skander.basly@gmail.com>
2020-04-26 14:35:11 +0100

    add models, repositories, services, controllers, app boutstrap, project lombok

---
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -0,0 +1,67 @@
+package guru.springframework.domain;
+
+import lombok.Data;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+public class Recipe {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Lob
+	private String description;
+	private Integer prepTime;
+	private Integer cookTime;
+	private Integer servings;
+	private String source;
+	private String url;
+
+	@Lob
+	private String directions;
+	private Byte[] image;
+
+	@Enumerated(value = EnumType.STRING)
+	private Difficulty difficulty;
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+	private Set<Ingredient> ingredients = new HashSet<>();
+
+	@OneToOne(cascade = CascadeType.ALL)
+	private Notes notes;
+
+	@ManyToMany
+	@JoinTable(name = "recipe_category",
+			joinColumns = @JoinColumn(name = "recipe_id"),
+			inverseJoinColumns = @JoinColumn(name = "category_id"))
+	Set<Category> categories = new HashSet<>();
+
+	public Recipe addIngredient(Ingredient ingredient) {
+		ingredient.setRecipe(this);
+		ingredients.add(ingredient);
+
+		return this;
+	}
+
+	public void setNotes(Notes notes) {
+		this.notes = notes;
+		notes.setRecipe(this);
+	}
+}
